3343753367c4d5986c076dfb8a5e799cc2c4919a
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-03 17:16:15 -0500

    Spawning Autos y maderas en un intervalo de tiempo random

---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour {
+    public GameObject prefab;
+    public float spawnIntervalMin = 2;
+    public float spawnIntervalMax = 6;
+    float nextSpawnTime = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time > nextSpawnTime)
+        {
+            Spawn();
+            nextSpawnTime = Time.time + Random.Range(spawnIntervalMin, spawnIntervalMax);
+        }
+    }
+
+    void Spawn() {
+        var instance = Instantiate(prefab, transform.position, transform.rotation, transform);
+    }
+}
