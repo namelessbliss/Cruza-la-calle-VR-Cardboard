bf455a4873dbeb288a89c37d806d00a276e70b04
namelessbliss <anonimo2911.2012@gmail.com>
2019-11-04 23:36:11 -0500

    Destructor, elimina los Rigidbody, optimizacion

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -7,6 +7,7 @@ public class Death : MonoBehaviour {
     public GameObject UICanvas;
 
     public void OnDeath() {
+        //acitva la interfaz con opcion de jugar de nuevo o salir
         UICanvas.SetActive(true);
         GetComponent<Rigidbody>().isKinematic = true;
     }
--- Assets/Scripts/Destructor.cs
+++ Assets/Scripts/Destructor.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class Destructor : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
-        Destroy(other.gameObject);
+        Destroy(other.GetComponentInParent<Rigidbody>().gameObject);
     }
 }
